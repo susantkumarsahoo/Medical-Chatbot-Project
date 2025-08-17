e18327abca37481f0914c6a57f2da8244390aabc
susantkumarsahoo <susantkumar.sks96@gmail.com>
2025-08-18 04:52:38 +0530

    file uplode

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Bappy Ahmed',
+    author= 'SUSANT KUMAR',
     author_email= 'susantkumar.sks96@gmail.com',
     packages= find_packages(),
     install_requires = []
